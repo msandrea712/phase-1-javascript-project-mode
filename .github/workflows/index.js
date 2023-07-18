4e0a6e5e3f13ce2c264cd61a63535517a0b16317
Andrea Cox <msandrea712@yahoo.com>
2023-07-18 15:05:40 -0400

    barbie

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,15 +1,4 @@
-const header = document.getElementById("header");
-const pokeId = document.getElementById("pokeId");
-const unorderedList = document.getElementsByTagName("ul")[0];
-const searchButton=document.getElementById("searchButton")
-searchButton.addEventListener("click",getPokemon)
- function getPokemon() {
- fetch(`https://pokeapi.co/api/v2/pokemon/${+pokeId.value}/`)
- .then(function(response){
-    return response.json()
- })
- .then(function(data){
-    unorderedList.innerText=data.name
- })
+function changeOutfit(item) {
+  const barbieElement = document.getElementById("barbie");
+  barbieElement.src = item;
 }
-
