517af4ba965c4c10d4c460a6c395fc1e98b7dc6f
Andrea Cox <msandrea712@yahoo.com>
2023-07-21 15:17:25 -0400

    tic tac toe

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -26,12 +26,12 @@ function checkWin() {
   const winPatterns = [
     [0, 1, 2],
     [3, 4, 5],
-    [6, 7, 8], // Rows
+    [6, 7, 8], 
     [0, 3, 6],
     [1, 4, 7],
-    [2, 5, 8], // Columns
+    [2, 5, 8], 
     [0, 4, 8],
-    [2, 4, 6], // Diagonals
+    [2, 4, 6], 
   ];
   return winPatterns.some((pattern) =>
     pattern.every((cellIndex) => board[cellIndex] === currentPlayer)
